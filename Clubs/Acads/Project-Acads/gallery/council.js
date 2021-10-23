ddc6c9582924b088c01d832f744f835a5900bbfd
Kanishk-00 <90696397+Kanishk-00@users.noreply.github.com>
2021-10-23 12:00:20 +0530

    Updated 2020 batch

--- Clubs/Acads/Project-Acads/gallery/council.js
+++ Clubs/Acads/Project-Acads/gallery/council.js
@@ -7,6 +7,8 @@ document.getElementById("gallery__btn2018").onclick = function () {
   document.getElementById("gallery__btn2019").style.backgroundColor = "white";
   document.getElementById("gallery__btn2017").style.color = "#505050";
   document.getElementById("gallery__btn2017").style.backgroundColor = "white";
+  document.getElementById("gallery__btn2020").style.color = "#505050";
+  document.getElementById("gallery__btn2020").style.backgroundColor = "white";
 
   //data change
 
@@ -35,6 +37,8 @@ document.getElementById("gallery__btn2019").onclick = function () {
   document.getElementById("gallery__btn2018").style.backgroundColor = "white";
   document.getElementById("gallery__btn2017").style.color = "#505050";
   document.getElementById("gallery__btn2017").style.backgroundColor = "white";
+  document.getElementById("gallery__btn2020").style.color = "#505050";
+  document.getElementById("gallery__btn2020").style.backgroundColor = "white";
 
   //data change
   document.querySelector(".gallery__crs .gallery__crs__mnc h4").innerHTML =
@@ -67,6 +71,8 @@ document.getElementById("gallery__btn2017").onclick = function () {
   document.getElementById("gallery__btn2019").style.backgroundColor = "white";
   document.getElementById("gallery__btn2018").style.color = "#505050";
   document.getElementById("gallery__btn2018").style.backgroundColor = "white";
+  document.getElementById("gallery__btn2020").style.color = "#505050";
+  document.getElementById("gallery__btn2020").style.backgroundColor = "white";
 
   //data change
 
@@ -87,4 +93,38 @@ document.getElementById("gallery__btn2017").onclick = function () {
   //element hide
   document.querySelector(".gallery__crs .gallery__crs__mnc").style.display =
     "none";
+};
+
+document.getElementById("gallery__btn2020").onclick = function () {
+  //color change
+  document.getElementById("gallery__btn2020").style.color = "white";
+  document.getElementById("gallery__btn2020").style.backgroundColor = "#505050";
+  document.getElementById("gallery__btn2018").style.color = "#505050";
+  document.getElementById("gallery__btn2018").style.backgroundColor = "white";
+  document.getElementById("gallery__btn2017").style.color = "#505050";
+  document.getElementById("gallery__btn2017").style.backgroundColor = "white";
+  document.getElementById("gallery__btn2019").style.color = "#505050";
+  document.getElementById("gallery__btn2019").style.backgroundColor = "white";
+
+  //data change
+  document.querySelector(".gallery__crs .gallery__crs__mnc h4").innerHTML =
+    "SANSKAR PRATAP";
+  document.querySelector(".gallery__crs .gallery__crs__cse h4").innerHTML =
+    "YOHAN KUMAR";
+  document.querySelector(".gallery__crs .gallery__crs__ee h4").innerHTML =
+    "ATHARVA BHANAGE";
+  document.querySelector(".gallery__crs .gallery__crs__me h4").innerHTML =
+    "PRASHANT CHOUDHARY";
+  //PHOTO CHANGE
+  document.querySelector(".gallery__crs .gallery__crs__mnc img").src =
+    "./Acads/photos/SanskarPratap_CR-MnC2020.jpeg";
+  document.querySelector(".gallery__crs .gallery__crs__cse img").src =
+    "./Acads/photos/C8B7A76D-8EEE-4F95-9B27-B221284ACC93 - YOHAN KUMAR.jpeg";
+  document.querySelector(".gallery__crs .gallery__crs__ee img").src =
+    "./Acads/photos/atharva_bhanage - ATHARVA BHANAGE.jpeg";
+  document.querySelector(".gallery__crs .gallery__crs__me img").src =
+    "./Acads/photos/Screenshot_2021-09-02-11-51-10-05 - PRASHANT CHOUDHARY.jpg";
+  //element hide
+  document.querySelector(".gallery__crs .gallery__crs__mnc").style.display =
+    "flex";
 };

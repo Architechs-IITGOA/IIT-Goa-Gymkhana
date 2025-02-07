ec086d5d35f94b605853bd3c14921d99de459bfe
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2025-02-07 22:35:38 +0530

    event section updated.

---
+++ Clubs/Sports/Javascript/event.js
@@ -0,0 +1,41 @@
+document.getElementById('event-card1').addEventListener("click", function() {
+    document.getElementById("popup13").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+  },);
+  document.querySelector('.close13').addEventListener("click", function() {
+    document.getElementById("popup13").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+  });
+  document.getElementById('event-card2').addEventListener("click", function() {
+    document.getElementById("popup14").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+  },);
+  document.querySelector('.close14').addEventListener("click", function() {
+    document.getElementById("popup14").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+  });
+  document.getElementById('event-card3').addEventListener("click", function() {
+    document.getElementById("popup15").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+  },);
+  document.querySelector('.close15').addEventListener("click", function() {
+    document.getElementById("popup15").style.display = "none";
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+  });
+  document.getElementById('event-card4').addEventListener("click", function() {
+    document.getElementById("popup16").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+  },);
+  document.querySelector('.close16').addEventListener("click", function() {
+    document.getElementById("popup16").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+  });
+  document.getElementById('event-card5').addEventListener("click", function() {
+    document.getElementById("popup17").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+  },);
+  document.querySelector('.close17').addEventListener("click", function() {
+    document.getElementById("popup17").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+  });

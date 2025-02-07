b472d4bcddfb0fd5641b5c1fb9e04c62663ecb5c
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2025-02-08 03:31:16 +0530

    sports performance section added.

---
+++ Clubs/Sports/Javascript/performance.js
@@ -0,0 +1,28 @@
+document.getElementById('perf-card1').addEventListener("click", function() {
+    document.getElementById("popup18").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+},);
+  document.querySelector('.close18').addEventListener("click", function() {
+    document.getElementById("popup18").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+});
+// -----------------------------------------
+  document.getElementById('perf-card2').addEventListener("click", function() {
+    document.getElementById("popup19").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+},);
+  document.querySelector('.close19').addEventListener("click", function() {
+    document.getElementById("popup19").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+});
+// -----------------------------------------
+  document.getElementById('perf-card3').addEventListener("click", function() {
+    document.getElementById("popup20").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+},);
+  document.querySelector('.close20').addEventListener("click", function() {
+    document.getElementById("popup20").style.display = "none";
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("navbar").style.display = "block";
+});
+// --------------------------------------------------

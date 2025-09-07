94353f7e54336db276e301a7f18786cd597ec2a1
Pranay Machhi <machhipranay07@gmail.com>
2025-09-08 02:03:46 +0530

    nav bar changed, js file to navigate slides

---
+++ Clubs/STAB/STAB/Javascript/pages.js
@@ -0,0 +1,37 @@
+
+let home = document.querySelector('#home');
+let aboutus = document.querySelector('#aboutus');
+let timeline = document.querySelector('#timeline');
+let contactus = document.querySelector('#contactus');
+
+home.addEventListener("click", ()=>{
+    let list = document.querySelectorAll('.section');
+    list.forEach((elem)=>{
+        elem.style.display = "none";
+    });
+    document.querySelector('#section0').style.display = "block";
+});
+
+aboutus.addEventListener("click", ()=>{
+    let list = document.querySelectorAll('.section');
+    list.forEach((elem)=>{
+        elem.style.display = "none";
+    });
+    document.querySelector('#section1').style.display = "block";
+});
+
+timeline.addEventListener("click", ()=>{
+    let list = document.querySelectorAll('.section');
+    list.forEach((elem)=>{
+        elem.style.display = "none";
+    });
+    document.querySelector('#section2').style.display = "block";
+});
+
+contactus.addEventListener("click", ()=>{
+    let list = document.querySelectorAll('.section');
+    list.forEach((elem)=>{
+        elem.style.display = "none";
+    });
+    document.querySelector('#section3').style.display = "block";
+});
